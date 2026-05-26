3f071a236cbba5a7a893be294b0c7070b8644c56
evarisk-kilyan <kilyan.evarisk@gmail.com>
2026-05-26 17:21:48 +0200

    fix: remove unused ArrowLeft import in NetBrut

--- src/pages/NetBrut.jsx
+++ src/pages/NetBrut.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { ArrowLeft } from 'lucide-react'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import Seo from '../components/Seo'
